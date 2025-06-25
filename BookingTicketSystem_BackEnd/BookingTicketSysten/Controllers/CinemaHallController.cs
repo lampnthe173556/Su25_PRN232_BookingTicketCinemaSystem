b1fa3f2d861efb44ca263b66e77497bd3f310c08
ADMIN <longhoangcj@gmail.com>
2025-06-25 19:07:14 +0700

    api cinema & cinema hall

---
+++ BookingTicketSystem_BackEnd/BookingTicketSysten/Controllers/CinemaHallController.cs
@@ -0,0 +1,36 @@
+﻿
+using BookingTicketSysten.Services.CinemaHallServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingTicketSysten.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CinemaHallController : ControllerBase
+    {
+        private readonly ICinemaHallService _cinemaHallService;
+
+        public CinemaHallController(ICinemaHallService cinemaHallService)
+        {
+            _cinemaHallService = cinemaHallService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWithSeats()
+        {
+            var result = await _cinemaHallService.GetAllCinemaHallsWithSeatsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _cinemaHallService.GetCinemaHallByIdAsync(id);
+
+            if (result == null)
+                return NotFound(new { message = "Cinema hall not found." });
+
+            return Ok(result);
+        }
+    }
+}
---
+++ BookingTicketSystem_BackEnd/BookingTicketSysten/Services/CinemaHallServices/ICinemaHallService.cs
@@ -0,0 +1,12 @@
+﻿using BookingTicketSysten.Models.DTOs.CinemaHallDTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookingTicketSysten.Services.CinemaHallServices
+{
+    public interface ICinemaHallService
+    {
+        Task<List<CinemaHallDto>> GetAllCinemaHallsWithSeatsAsync();
+        Task<CinemaHallDto?> GetCinemaHallByIdAsync(int hallId);
+    }
+}
